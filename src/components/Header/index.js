63d420cd4456bbe4a15b295f987d27a34498b524
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-03-08 17:36:32 -0300

    Incluindo Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Container } from './styles';
+import {Container, Top, Logo, Title} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import logo from '~/assets/Nubank_Logo.png';
 
 export default function Header() {
   return (
     <Container>
       <Top>
-        <Logo source={} />
+        <Logo source={logo} />
         <Title>Wermeson</Title>
       </Top>
+      <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
     </Container>
   );
 }
--- src/components/Header/style.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  align-items: center;
-  padding: 40px 0 30px;
-`;
