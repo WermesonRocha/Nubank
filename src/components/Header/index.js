ec7ab0628094292502c931697f4c538060c0cb36
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-03-08 17:07:36 -0300

    Removendo pasta node_modules

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Top>
+        <Logo source={} />
+        <Title>Wermeson</Title>
+      </Top>
+    </Container>
+  );
+}
---
+++ src/components/Header/style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  padding: 40px 0 30px;
+`;
