e159df1776dfedafe3800749a7e9bc6a3df19e05
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-03-08 18:18:01 -0300

    Adicionado footer menu de rolagem lateral

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,6 +1,56 @@
 import React from 'react';
-import {Container} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container, TabsContainer, TabItem, TabText} from './styles';
 
 export default function Tabs() {
-  return <Container />;
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#FFF" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="smartphone" size={24} color="#FFF" />
+          <TabText>Recarga de celular</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="attach-money" size={24} color="#FFF" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="credit-card" size={24} color="#FFF" />
+          <TabText>Empréstimos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-downward" size={24} color="#FFF" />
+          <TabText>Depositar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-upward" size={24} color="#FFF" />
+          <TabText>Transferir</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="tune" size={24} color="#FFF" />
+          <TabText>Ajustar limite</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="help" size={24} color="#FFF" />
+          <TabText>Me ajuda</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="line-style" size={24} color="#FFF" />
+          <TabText>Pagar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="lock" size={24} color="#FFF" />
+          <TabText>Bloquear cartão</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="payment" size={24} color="#FFF" />
+          <TabText>Cartão virtual</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
 }
