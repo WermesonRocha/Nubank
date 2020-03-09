531bb2add6c6cfd90ca368bab3d7d0ca80ea1665
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-03-08 21:22:42 -0300

    Adicionado card com conteúdo principal do app

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
-import {Container} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 15.681,77</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 55,00 recebida de Werton Fabian Rocha da Silva
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
       <Tabs />
     </Container>
   );
